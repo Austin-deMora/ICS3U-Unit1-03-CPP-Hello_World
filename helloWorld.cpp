a9c77f9df211774061553ea36aa545e3b1550b11
Your Name <you@example.com>
2021-04-23 14:43:04 +0000

    Initial commit

---
+++ helloWorld.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2021 Austin de Mora All rights reserved
+//
+// Created by: Austin de Mora
+// Created on: April 2021
+// This is the "Hello, World!" Program, with proper style
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	std::cout << "Goodbye, World!" << std::endl;
+}
